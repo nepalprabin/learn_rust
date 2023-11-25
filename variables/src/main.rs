c51665cba4735e195b2c923295d73f8729c7e608
Prabin Nepal <prabinnepal1996@gmail.com>
2023-11-24 20:14:30 -0600

    Ahadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let x = x + 3;
+    println!("The value of x after shadowing is: {x}");
 }
