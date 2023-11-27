ade2f41b0c7d5deab85c6ad7cda2369e924a54e6
Prabin Nepal <prabinnepal1996@gmail.com>
2023-11-27 09:49:02 -0600

    Return values from loops

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -10,4 +10,15 @@ fn main() {
     let condition = true;
     let n = if condition {1} else {2};
     println!("The value of n is: {n}");
+
+    // Repeating code with loop
+    let mut counter = 0;
+    let result = loop {
+        println!("Entering into loop!");
+        counter += 1;
+        if counter == 10 {
+            break counter;
+        }
+    };
+    println!("Result is: {result}")
 }
