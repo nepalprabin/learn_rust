d8611ae93fcb5e31124ef45669c88a7539dc06af
Prabin Nepal <prabinnepal1996@gmail.com>
2023-11-27 09:18:27 -0600

    Function with return values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     println!("Hello, world!");
     alarm_function(5, "hours");
+    let y = return_value_fn();
+    println!("The value from return_value_fn() is: {y}");
 }
 
 fn alarm_function(unit: i32, unit_label: &str) {
     println!("Wake up after: {unit} {unit_label}");
+}
+
+fn return_value_fn() -> i32 {
+    10
 }
