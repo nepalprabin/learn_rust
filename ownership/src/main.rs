1cd89370bac716d0302b75af259b60b5ded68a7d
Prabin Nepal <prabinnepal1996@gmail.com>
2023-11-28 02:48:57 -0600

    Interacting with clone

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     let s2 = s1;
     println!("{} world", s2);
 
+    // Variables and Data Interacting with Clone
+    let s3 = String::from("hello");
+    let s4 = s3.clone();
+    println!("s1={}, s2={}", s3, s4);
+
 }
